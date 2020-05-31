df662ff3489eb61e24b7695636976b07d3248033
Dipen Kumar <dipenkumar@Dipens-MacBook-Air.local>
2020-05-31 23:35:04 +0545

    update

---
+++ index1.js
@@ -0,0 +1,33 @@
+function postToGoogle() {
+
+
+                var username = $("#username").val();
+                username = username + ".html";
+                window.open(username);
+            //     const fs = require('fs')
+            //     alert("Heyy");
+            //     return true;
+            //     console.log("Manoj");
+            //     console.log("Manoj");
+            //     fs.readFile('users.txt', 'utf-8', (err, data) => { 
+            //   if (err) throw err; 
+
+            //   var array = data.split(" ");
+            //   var i;
+            //   for(i = 0; i<array.length; i++)
+            //   {
+            //     var n = data.localeCompare(array[i]);
+            //     if(n == 0)
+            //     {
+            //       var str = array[i] + ".html";
+            //       alert("Yupp");
+            //       window.open(str);
+            //       return true;
+            //     }
+            //   }
+            //   alert("Given user is not on BeMotivated");
+            //   return false;
+            // });
+  
+            }
+
--- post.js
+++ post.js
@@ -13,7 +13,7 @@ function postToGoogle() {
           },
           error: function(x, y, z)
             {
-              alert("Your story has been posted.")
+              alert("Your story has been posted.");
               window.open("index.html", "_top");
             }
                 });
