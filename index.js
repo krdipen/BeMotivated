a391c51faa2550e2b53af6214eceb4a4bd932959
Dipen Kumar <dipenkumar@Dipens-MacBook-Air.local>
2020-06-01 04:39:41 +0545

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-function postToGoogle() {
-
-
-                var username = $("#username").val();
-                username = "https://krdipen.github.io/BeMotivated/" + username;
-                window.open("https://krdipen.github.io/BeMotivated/signup","_top");
-
-            }
